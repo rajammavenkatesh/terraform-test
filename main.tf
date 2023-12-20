ff8c0c401c208ef21a5d5461cee76f58d18b6032
Dr.Raj <dr.raj@Admins-MacBook-Air.local>
2023-12-20 13:13:48 +0530

    added custom vpc

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
     Name = "HelloWorld"

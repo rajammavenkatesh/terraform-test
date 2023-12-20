ed168818493b21dbdea42d38de65fcc40ec8dc93
Dr.Raj <dr.raj@Admins-MacBook-Air.local>
2023-12-20 12:49:32 +0530

    Fixed syntax issue

--- main.tf
+++ main.tf
@@ -17,7 +17,8 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
-}
+
   tags = {
     Name = "HelloWorld"
   }
+}
